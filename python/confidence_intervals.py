f877f889f725cf78c94d5c252980a39c577d5978
Justin1904 <justin1904@live.com>
2019-02-09 14:38:06 -0500

    Added paired percentile tests and the unit tests for them.

--- python/confidence_intervals.py
+++ python/confidence_intervals.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy
 import math
+from pdb import set_trace
 
 from sklearn.utils import resample
 
@@ -16,9 +17,19 @@ def percentile(preds, labels, score_func, confidence_level, sample_size, num_boo
         subset_scores = score_func(subset_preds, subset_labels)
         scores.append(subset_scores)
     scores = sorted(scores)
-    lower = int(math.ceil(num_bootstrap * (1-confidence_level)))
-    upper = int(math.floor(num_bootstrap * confidence_level))
-    return scores[lower], scores[upper]
+    lower = np.quantile(scores, (1-confidence_level)/2)
+    upper = np.quantile(scores, (1+confidence_level)/2)
+    return lower, upper
 
-def paired(scores1, scores2, confidence_level):
-    raise NotImplementedError
+def paired_percentile(preds1, preds2, labels, score_func, confidence_level, sample_size, num_bootstrap):
+    scores = []
+    while len(scores) < num_bootstrap:
+        subset_preds1, subset_preds2, subset_labels = resample(preds1, preds2, labels, n_samples=sample_size)
+        subset_scores1 = score_func(subset_preds1, subset_labels)
+        subset_scores2 = score_func(subset_preds2, subset_labels)
+        subset_scores = subset_scores2 - subset_scores1
+        scores.append(subset_scores)
+    lower = np.quantile(scores, (1-confidence_level)/2)
+    upper = np.quantile(scores, (1+confidence_level)/2)
+    set_trace()
+    return lower, upper
