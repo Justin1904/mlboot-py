4cbe91e827b5da481495b1bd6fed30c970cd619d
Justin1904 <Justin1904@live.com>
2020-07-05 01:04:10 -0700

    Major overhaul: 1) Add multiprocessing optimization to make things faster; 2) Add support for irreducible score_fn; 3) Add the flag for reducible or not.

--- mlboot/confidence_intervals.py
+++ mlboot/confidence_intervals.py
@@ -1,24 +1,47 @@
+from mlboot.utils import resample_and_average, resample_and_score
 import numpy as np
 import scipy
-import math
-
-from pdb import set_trace
-from sklearn.utils import resample
+from joblib import Parallel, delayed
 
 
-def bootstrap_avg(samples, n_bootstrap, n_samples):
+def bootstrap_avg_score(samples, n_bootstrap, n_samples, n_workers):
     """
     Bootstrap `n_bootstrap` sub-samples from `samples` each with sample size `n_samples`,
     average them and return.
     """
-    avg_score_samples = []
-    while len(avg_score_samples) < n_bootstrap:
-        sample_scores = resample(samples, n_samples=n_samples)
-        avg_score_samples.append(sample_scores.mean())
+    avg_score_samples = Parallel(n_jobs=n_workers)(
+        delayed(resample_and_average)(samples, n_samples=n_samples)
+        for _ in range(n_bootstrap)
+    )
     return avg_score_samples
 
 
-def jackknife_avg(samples):
+def bootstrap_compute_score(y_pred, y_true, score_fn, n_bootstrap, n_samples, n_workers):
+    """
+    Bootstrap 'n_bootstrap' sub-samples from `y_pred` and 'y_true`, each with sample size `n_samples`,
+    use `score_fn` to compute a single-scalar score for each sub-sample and return.
+    """
+    computed_score_samples = Parallel(n_jobs=n_workers)(
+        delayed(resample_and_score)(y_pred, y_true, score_fn, n_samples=n_samples)
+        for _ in range(n_bootstrap)
+    )
+    return computed_score_samples
+
+
+def bootstrap_diff_score(y_pred, y_pred_baseline, y_true, score_fn, n_bootstrap, n_samples, n_workers):
+    """
+    Bootstrap 'n_bootstrap' sub-samples from `y_pred`, `y_pred_baseline` and 'y_true`, each with sample size `n_samples`,
+    use `score_fn` to compute a single-scalar score for each sub-sample, take their difference and return.
+    """
+    diff_score_samples = Parallel(n_jobs=n_workers)(
+        delayed(resample_and_score)(y_pred, y_true, score_fn, n_samples=n_samples) -
+        delayed(resample_and_score)(y_pred_baseline, y_true, score_fn, n_samples=n_samples)
+        for _ in range(n_bootstrap)
+    )
+    return diff_score_samples
+
+
+def jackknife_avg_score(samples):
     """
     Jackknife (leave-one-out resampling) sub-samples from `samples`,
     comptue their average and return.
@@ -31,6 +54,33 @@ def jackknife_avg(samples):
     return jackknife_samples
 
 
+def jackknife_compute_score(y_pred, y_true, score_fn, n_workers):
+    """
+    Jackknife (leave-one-out resampling) sub-samples from `y_pred` and `y_true`,
+    use `score_fn` to comptue the scores and return.
+    """
+    n_samples = y_true.shape[0]
+    jackknife_samples = Parallel(n_jobs=n_workers)(
+        delayed(score_fn)(y_pred[np.arange(n_samples) != i], y_true[np.arange(n_samples) != i])
+        for i in range(n_samples)
+    )
+    return jackknife_samples
+
+
+def jackknife_diff_score(y_pred, y_pred_baseline, y_true, score_fn, n_workers):
+    """
+    Jackknife (leave-one-out resampling) sub-samples from `y_pred`, `y_pred_baseline` and `y_true`,
+    use `score_fn` to comptue the scores for `y_pred` and `y_true`, take their difference and return.
+    """
+    n_samples = y_true.shape[0]
+    jackknife_samples = Parallel(n_jobs=n_workers)(
+        delayed(score_fn)(y_pred[np.arange(n_samples) != i], y_true[np.arange(n_samples) != i]) -
+        delayed(score_fn)(y_pred_baseline[np.arange(n_samples) != i], y_true[np.arange(n_samples) != i])
+        for i in range(n_samples)
+    )
+    return jackknife_samples
+
+
 def bca_bias_correction(scores, full_score):
     scores = np.array(scores)
     entries_less = (scores < full_score).sum()
@@ -40,8 +90,7 @@ def bca_bias_correction(scores, full_score):
     return bias_correction
 
 
-def bca_accel_param(samples):
-    jackknife_scores = jackknife_avg(samples)
+def bca_accel_param(jackknife_scores):
     jackknife_avg_scores = jackknife_scores.mean()
     accel_numerator = ((jackknife_avg_scores - jackknife_scores) ** 3).sum()
     accel_denominator = ((jackknife_avg_scores - jackknife_scores) ** 2).sum()
@@ -52,7 +101,7 @@ def bca_accel_param(samples):
 def compute_bca_quantiles(confidence_level, bias_correction, accel_param):
     alpha = 1 - confidence_level
     normal = scipy.stats.norm()
-    
+
     zl = bias_correction + normal.ppf(alpha / 2)
     alpha1 = normal.cdf(bias_correction + zl / (1 - accel_param * zl))
 
@@ -61,28 +110,47 @@ def compute_bca_quantiles(confidence_level, bias_correction, accel_param):
     return alpha1, alpha2
 
 
-def bca_estimator(pointwise_scores, confidence_level, n_bootstrap, n_samples):
+def bca_estimator(confidence_level, n_bootstrap, n_samples, n_workers, pointwise_scores=None, y_pred=None, y_pred_baseline=None, y_true=None, score_fn=None):
     """
     Bias-corrected-accelerated estimator. More accurate and sample efficient.
     """
-    sample_scores = bootstrap_avg(pointwise_scores, n_bootstrap, n_samples)
-    global_avg_score = pointwise_scores.mean()
-    bias_correction = bca_bias_correction(sample_scores, global_avg_score)
-    accel_param = bca_accel_param(pointwise_scores)
+    if pointwise_scores is not None:
+        sample_scores = bootstrap_avg_score(pointwise_scores, n_bootstrap, n_samples, n_workers)
+        global_score = pointwise_scores.mean()
+        jackknife_scores = jackknife_avg_score(pointwise_scores)
+    elif y_pred_baseline is None:
+        sample_scores = bootstrap_compute_score(y_pred, y_true, score_fn, n_bootstrap, n_samples, n_workers)
+        global_score = score_fn(y_pred, y_true)
+        jackknife_scores = jackknife_compute_score(y_pred, y_true, score_fn, n_workers)
+    else:
+        sample_scores = bootstrap_diff_score(y_pred, y_pred_baseline, y_true, score_fn, n_bootstrap, n_samples, n_workers)
+        global_score = score_fn(y_pred, y_true) - score_fn(y_pred_baseline, y_true)
+        jackknife_scores = jackknife_diff_score(y_pred, y_pred_baseline, y_true, score_fn, n_workers)
+
+    bias_correction = bca_bias_correction(sample_scores, global_score)
+    accel_param = bca_accel_param(jackknife_scores)
     quantile_lo, quantile_hi = compute_bca_quantiles(confidence_level, bias_correction, accel_param)
     lo = np.quantile(sample_scores, quantile_lo)
     hi = np.quantile(sample_scores, quantile_hi)
     return lo, hi, sample_scores
 
 
-def percentile_estimator(pointwise_scores, confidence_level, n_bootstrap, n_samples):
+def percentile_estimator(confidence_level, n_bootstrap, n_samples, n_workers, pointwise_scores=None, y_pred=None, y_pred_baseline=None, y_true=None, score_fn=None):
     """
     Vanilla percentile estimator. Takes the quantile position's value in sorted scores.
     """
-    sample_scores = bootstrap_avg(pointwise_scores, n_bootstrap, n_samples)
-    lo = np.quantile(sample_scores, (1 - confidence_level)/2)
-    hi = np.quantile(sample_scores, (1 + confidence_level)/2)
-    return lo, hi, sample_scores
+    if pointwise_scores is not None:
+        sample_scores = bootstrap_avg_score(pointwise_scores, n_bootstrap, n_samples, n_workers)
+        global_score = pointwise_scores.mean()
+    elif y_pred_baseline is None:
+        sample_scores = bootstrap_compute_score(y_pred, y_true, score_fn, n_bootstrap, n_samples, n_workers)
+        global_score = score_fn(y_pred, y_true)
+    else:
+        sample_scores = bootstrap_diff_score(y_pred, y_pred_baseline, y_true, score_fn, n_bootstrap, n_samples, n_workers)
+        global_score = score_fn(y_pred, y_true) - score_fn(y_pred_baseline, y_true)
+    lo = np.quantile(sample_scores, (1 - confidence_level) / 2)
+    hi = np.quantile(sample_scores, (1 + confidence_level) / 2)
+    return lo, hi, sample_scores, global_score
 
 
 # def cluster_percentile(preds, labels, score_func, cluster, confidence_level, sample_size, num_bootstrap):
--- mlboot/mlboot.py
+++ mlboot/mlboot.py
@@ -1,14 +1,8 @@
 import numpy as np
-import scipy
-import sklearn
-
-from sklearn.utils import resample
 from mlboot.confidence_intervals import (
     percentile_estimator,
     bca_estimator,
 )
-
-from pdb import set_trace
 
 
 CI_METHOD_DICT = {
@@ -23,12 +17,25 @@ class ConfidenceIntervalEstimator:
     confidence interval of performance of a single model.
 
     Args:
-         - score_fn [Callable]: a scoring function that scores each prediction and each ground truth. An important assumption is that 
+         - score_fn [Callable]: a scoring function that scores each prediction and each ground truth. An important assumption is that
                                 the overall score of a set of predictions will be the average of scores of each prediction.
          - method [str]: 'bca'/'percentile', the method to choose for estimating the confidence interval, default to 'bca'.
          - confidence_level [float]: the confidence level of the estimation. The more confidence, the wider the interval. Default to 0.95.
          - n_bootstrap [int]: the number of Bootstrap sampling to do for estimating the confidence interval. Default to 2000.
          - n_samples [int]: the sample size for each Bootstrap sampling. Default to None, will use y_pred.shape[0] as n_samples in this case.
+         - n_workers [int]: the number of workers in any multi-processing scenarios. Defaults to 4.
+         - reducible [bool]: when set to True, there are some optimizations to make internal computations faster. CAVEATS APPLY:
+
+                             NOTE: reducible=True can only work correctly when:
+                             1) the population-wide score can be represented as an average over score-per-datapoint
+                                (e.g. accuracy is the average of 0-1 score, MSE is the average of squared error for each datapoint);
+                             2) score_fn should return the score-per-datapoint in an 1-D array, rather than directly
+                                returning the population-wide score.
+
+                             Violating 2) would trigger an error, while violating 1) would simply give you incorrect results.
+                             If you are unsure what this means, keep reducible=False to ensure correctness. Note that not all
+                             metrics are reducible, examples are recall, precision, F1, where the population-wide metric is not
+                             in the form of an average over score-per-datapoint.
 
     Input:
          - y_pred [np.ndarray]: an array of predictions, batch dimension must be on the first dimension.
@@ -36,7 +43,7 @@ class ConfidenceIntervalEstimator:
          - return_samples [bool]: whether or not to return the scores on Bootstrap-sampled subsets.
 
     Return:
-         - global_avg_score: the average score over the entire set of predictions.
+         - global_score: the average score over the entire set of predictions.
          - (ci_lo, ci_hi): a tuple of lower end and higher end of the confidence interval.
          - avg_score_samples (Optional): the average scores on the Bootstrap-sampled subsets.
     """
@@ -48,11 +55,15 @@ class ConfidenceIntervalEstimator:
         confidence_level=0.95,
         n_bootstrap=2000,
         n_samples=None,
+        n_workers=4,
+        reducible=False,
     ):
         self.score_fn = score_fn
         self.confidence_level = confidence_level
         self.n_bootstrap = n_bootstrap
         self.n_samples = n_samples  # if set to None, will be dynamically set as it processes input of different sizes
+        self.n_workers = n_workers
+        self.reducible = reducible
         self.method = method
         self.estimator = CI_METHOD_DICT[method]
 
@@ -64,26 +75,29 @@ class ConfidenceIntervalEstimator:
         assert pointwise_scores.shape[0] == len(
             y_pred
         ), f"score_fn output has {pointwise_scores.shape[0]} entries, yet y_pred has {y_pred.shape[0]} entries"
-        global_avg_score = pointwise_scores.mean()
-        return pointwise_scores, global_avg_score
-
-    def _sample_avg_scores(self, scores):
-        # TODO: Figure out a more efficient method for this function
-        avg_score_samples = []
-        n_samples = scores.shape[0] if self.n_samples is None else self.n_samples
-        while len(avg_score_samples) < self.n_bootstrap:
-            score_samples = resample(scores, n_samples=n_samples)
-            avg_score_samples.append(score_samples.mean())
-        return avg_score_samples
+        return pointwise_scores
 
     def __call__(self, y_pred, y_true, return_samples=False):
-        pointwise_scores, global_avg_score = self._compute_scores(y_pred, y_true)
-        # avg_score_samples = self._sample_avg_scores(pointwise_scores)
-        ci_lo, ci_hi, avg_score_samples = self.estimator(pointwise_scores, self.confidence_level, self.n_bootstrap, self.n_samples)
-        if return_samples:
-            return global_avg_score, (ci_lo, ci_hi), avg_score_samples
+        if self.reducible:
+            pointwise_scores = self._compute_scores(y_pred, y_true)
         else:
-            return global_avg_score, (ci_lo, ci_hi)
+            pointwise_scores = None
+
+        n_samples = y_pred.shape[0] if self.n_samples is None else self.n_samples
+        ci_lo, ci_hi, avg_score_samples, global_score = self.estimator(
+            confidence_level=self.confidence_level,
+            n_bootstrap=self.n_bootstrap,
+            n_samples=n_samples,
+            pointwise_scores=pointwise_scores,
+            y_pred=y_pred,
+            y_true=y_true,
+            score_fn=self.score_fn,
+        )
+
+        if return_samples:
+            return global_score, (ci_lo, ci_hi), avg_score_samples
+        else:
+            return global_score, (ci_lo, ci_hi)
 
 
 class PairedConfidenceIntervalEstimator(ConfidenceIntervalEstimator):
@@ -97,28 +111,35 @@ class PairedConfidenceIntervalEstimator(ConfidenceIntervalEstimator):
          - return_samples [bool]: whether or not to return the scores on Bootstrap-sampled subsets.
 
     Return:
-         - delta_global_avg_score: the average score delta over the entire set of predictions. Positive means score_<ours> > score_<baseline>.
+         - delta_global_score: the average score delta over the entire set of predictions. Positive means score_<ours> > score_<baseline>.
          - (ci_lo, ci_hi): a tuple of lower end and higher end of the confidence interval.
          - avg_delta_score_samples (Optional): the average score deltas on the Bootstrap-sampled subsets.
     """
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-
     def __call__(self, y_pred_ours, y_pred_baseline, y_true, return_samples=False):
-        pointwise_scores_ours, global_avg_score_ours = self._compute_scores(
+        pointwise_scores_ours = self._compute_scores(
             y_pred_ours, y_true
         )
-        pointwise_scores_baseline, global_avg_score_baseline = self._compute_scores(
+        pointwise_scores_baseline = self._compute_scores(
             y_pred_baseline, y_true
         )
+
+        n_samples = y_pred_ours.shape[0] if self.n_samples is None else self.n_samples
         delta_pointwise_scores = pointwise_scores_ours - pointwise_scores_baseline
-        delta_global_avg_score = global_avg_score_ours - global_avg_score_baseline
-        ci_lo, ci_hi, avg_delta_score_samples = self.estimator(delta_pointwise_scores, self.confidence_level, self.n_bootstrap, self.n_samples)
+        ci_lo, ci_hi, avg_delta_score_samples, delta_global_score = self.estimator(
+            confidence_level=self.confidence_level,
+            n_bootstrap=self.n_bootstrap,
+            n_samples=n_samples,
+            pointwise_scores=delta_pointwise_scores,
+            y_pred=y_pred_ours,
+            y_pred_baseline=y_pred_baseline,
+            y_true=y_true,
+            score_fn=self.score_fn,
+        )
         if return_samples:
-            return delta_global_avg_score, (ci_lo, ci_hi), avg_delta_score_samples
+            return delta_global_score, (ci_lo, ci_hi), avg_delta_score_samples
         else:
-            return delta_global_avg_score, (ci_lo, ci_hi)
+            return delta_global_score, (ci_lo, ci_hi)
 
 
 class ClusteredConfidenceIntervalEstimator(ConfidenceIntervalEstimator):
